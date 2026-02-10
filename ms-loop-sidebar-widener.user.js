36e8f1153a75f595d622ceba0d14e88b461c58b5
Cary Symes <CSymes@users.noreply.github.com>
2026-02-10 16:22:32 +1100

    Add pre-release Loop domain

--- ms-loop-sidebar-widener.user.js
+++ ms-loop-sidebar-widener.user.js
@@ -5,6 +5,7 @@
 // @description  Make the Loop sidebar (slightly) less suckish
 // @author       Cary Symes
 // @match        https://loop.cloud.microsoft/p/*
+// @match        https://loop.cloud-dev.microsoft/p/*
 // @icon         https://res.cdn.office.net/fluid/prod/loop-app/hashed/assets/favicon/favicon-small.d141cdc47e.png
 // @downloadURL  https://github.com/CSymes/ms-loop-sidebar-widener/raw/refs/heads/main/ms-loop-sidebar-widener.user.js
 // @updateURL    https://github.com/CSymes/ms-loop-sidebar-widener/raw/refs/heads/main/ms-loop-sidebar-widener.user.js
